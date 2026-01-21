b663e5a82dbd6feb1fd7f8c23e7a5ed6f0adf1b8
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-21 17:33:39 +0100

    Aggiunto file style.css per la gestione dello stile delle card e incluso nel layout di index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ require_once "./server.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Web-app-dischi</title>
 </head>
 
