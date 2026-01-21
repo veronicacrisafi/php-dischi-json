0819096e6df20ce1a540c560beee66d6f5c87118
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-21 17:01:19 +0100

    Ristrutturato il layout della pagina index.php per visualizzare i dischi in un formato a card e aggiornate le istruzioni nel file instruction.txt.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ error_reporting(E_ALL);
 
 <?php
 require_once "./server.php";
-
 ?>
 
 
@@ -21,21 +20,26 @@ require_once "./server.php";
 </head>
 
 <body>
+
     <div class="container">
-        <h1>Dischi</h1>
-
-        <ul>
+        <h1 class="text-center my-4">Dischi</h1>
+        <div class="row">
             <?php foreach ($dischi as $disco) { ?>
-                <li>
-                    <img src="<?php echo $disco['URL']; ?>" alt="cover">
-                    <strong><?php echo $disco['title']; ?></strong><br>
-                    Artista: <?php echo $disco['artist']; ?><br>
-                    Anno: <?php echo $disco['pubblication-year']; ?><br>
-                    Genere: <?php echo $disco['genre']; ?>
-                </li>
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <img src="<?php echo $disco['URL']; ?>" class="card-img-top" alt="cover">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $disco['title']; ?></h5>
+                            <p class="card-text">
+                                <strong>Artista:</strong> <?php echo $disco['artist']; ?><br>
+                                <strong>Anno:</strong> <?php echo $disco['pubblication-year']; ?><br>
+                                <strong>Genere:</strong> <?php echo $disco['genre']; ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             <?php } ?>
-        </ul>
-
+        </div>
     </div>
 </body>
 
