1aceb4dc74c2d9f6c0f2a4c6aea9c5804f9154af
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-21 15:22:32 +0100

    Aggiunto file index.php e server.php; aggiornate le istruzioni per la comunicazione con lo script PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Web-app-dischi</title>
+</head>
+
+<body>
+    <h1>Dischi</h1>
+</body>
+
+</html>
