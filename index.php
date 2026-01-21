d21a00ce410baa3df7183c6e605b06bd6901afb9
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-01-21 19:10:07 +0100

    Aggiornato il layout di index.php con l'aggiunta di un logo musicale e un titolo per il modulo di aggiunta dischi. Modificato style.css per includere stili per il corpo e migliorare la tipografia.

--- index.php
+++ index.php
@@ -17,13 +17,21 @@ require_once "./server.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=DynaPuff:wght@400..700&family=Fascinate+Inline&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Permanent+Marker&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Web-app-dischi</title>
 </head>
 
 <body>
 
     <div class="container">
-        <h1 class="text-center my-4">Dischi</h1>
+        <div class="row align-items-start">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/031/737/196/small/spotify-icon-spotify-social-media-logo-free-png.png"
+                alt="music-logo"
+                class="my-4 float-start"
+                style="width:90px; height:auto;">
+        </div>
         <div class="row border rounded mb-3 py-4">
             <?php foreach ($dischi as $disco) { ?>
                 <div class="col-md-4 mb-4">
@@ -43,9 +51,9 @@ require_once "./server.php";
         </div>
 
 
+        <h1 class="my-4 text-center">Form per aggiungere un nuovo disco</h1>
         <form action="server.php" method="POST" class="my-5">
             <div class="card p-4">
-                <h4 class="mb-4">Aggiungi un nuovo disco</h4>
                 <div class="mb-3">
                     <label for="title" class="form-label">Titolo</label>
                     <input type="text" id="title" name="title" class="form-control" placeholder="Titolo canzone..." required>
